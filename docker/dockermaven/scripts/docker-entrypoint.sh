6b5bf8997a14bd4d80275181d874cba6b6e41564
Aldrin Piri <aldrinpiri@gmail.com>
2017-06-26 14:21:56 -0400

    Adjusting credentials to match password for MySQL that is established.  Adjusting DDL to use GARCON as database.  Introducing polling to entrypoint script to verify MySQL is started before starting Dropwizard application.
    This closes #5.

--- docker/dockermaven/scripts/docker-entrypoint.sh
+++ docker/dockermaven/scripts/docker-entrypoint.sh
@@ -2,15 +2,24 @@
 
 echo "This is the docker entrypoint script for Apache NiFi Device Registry that will start up all of the required components."
 
-nohup java -jar $NIFI_DEVICE_REGISTRY_BASE_DIR/nifi-device-registry-web-1.2.0-SNAPSHOT.jar server $NIFI_DEVICE_REGISTRY_BASE_DIR/Garcon.yml &
-/opt/nifi/nifi-1.2.0/bin/nifi.sh start
-
 # Set MySQL environment parameters
 export MYSQL_ROOT_PASSWORD=nifidevregistry
 export MYSQL_DATABASE=DEVICE_REGISTRY
 
 # Start MySQL
 /scripts/mysql.sh mysqld --user=root &
+
+/opt/nifi/nifi-1.2.0/bin/nifi.sh start
+
+# Let MySQL finish starting
+while ! mysqlshow --user=root --password=nifidevregistry; do
+    echo "MySQL not yet started... waiting"
+    sleep 2
+done
+
+echo "Starting Device Registry UI"
+
+nohup java -jar $NIFI_DEVICE_REGISTRY_BASE_DIR/nifi-device-registry-web-1.2.0-SNAPSHOT.jar server $NIFI_DEVICE_REGISTRY_BASE_DIR/Garcon.yml &
 
 while true; do
   sleep 30
