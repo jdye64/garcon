1f4efbf367b0af25c7849b57508ff6e8a7f549cd
Aldrin Piri <aldrinpiri@gmail.com>
2017-06-26 12:36:41 -0400

    Adjust DeviceRegistry.yml to be Garcon.yml and change mirror for NiFi binary as downloads were hanging.

--- docker/dockermaven/scripts/docker-entrypoint.sh
+++ docker/dockermaven/scripts/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "This is the docker entrypoint script for Apache NiFi Device Registry that will start up all of the required components."
 
-nohup java -jar $NIFI_DEVICE_REGISTRY_BASE_DIR/nifi-device-registry-web-1.2.0-SNAPSHOT.jar server $NIFI_DEVICE_REGISTRY_BASE_DIR/DeviceRegistry.yml &
+nohup java -jar $NIFI_DEVICE_REGISTRY_BASE_DIR/nifi-device-registry-web-1.2.0-SNAPSHOT.jar server $NIFI_DEVICE_REGISTRY_BASE_DIR/Garcon.yml &
 /opt/nifi/nifi-1.2.0/bin/nifi.sh start
 
 # Set MySQL environment parameters
